d73314cf9859fc81e30db5c6a0ae804709f07059
Yaroslav Oslam <ggstrider1@gmail.com>
2024-01-22 11:44:35 +0200

    Added simple model of data

---
+++ Assets/Model/GameSession.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Autophobia.Model
+{
+    public class GameSession : MonoBehaviour
+    {
+        [SerializeField] private PlayerData _data;
+        public PlayerData Data => _data;
+
+        private void Awake()
+        {
+            if (IsSessionsExist())
+            {
+                DestroyImmediate(gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(this);
+            }
+        }
+
+        private bool IsSessionsExist()
+        {
+            var sessions = FindObjectsOfType<GameSession>();
+
+            foreach (var session in sessions)
+            {
+                if (session != this) return true;
+            }
+
+            return false;
+        }
+    }
+}
