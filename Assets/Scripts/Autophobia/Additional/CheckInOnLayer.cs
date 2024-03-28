5a8c3ac4aa6b51daf81e29ec2fd3c7ef4cdff550
Yaroslav Oslam <ggstrider1@gmail.com>
2024-03-28 12:05:38 +0200

    a little updates

---
+++ Assets/Scripts/Autophobia/Additional/CheckInOnLayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace Autophobia.Additional
+{
+    public class CheckInOnLayer : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _layerToCheck;
+        [SerializeField] private float _radius;
+        
+        [SerializeField] private bool _isOnLayer;
+        public bool IsColliderOnLayer => _isOnLayer;
+
+        [FormerlySerializedAs("_eventOnTrue")] public UnityEvent EventOnTrue;
+        [FormerlySerializedAs("_eventOnFalse")] public UnityEvent EventOnFalse;
+
+        private void OnTriggerStay(Collider other)
+        {
+            _isOnLayer = Physics.CheckSphere(transform.position, _radius, _layerToCheck);
+            EventOnTrue?.Invoke();
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            _isOnLayer = Physics.CheckSphere(transform.position, _radius, _layerToCheck);
+            EventOnFalse?.Invoke();
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(transform.position, _radius);
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Autophobia/Dialogues/LuciasWalkAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Autophobia.Additional;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Autophobia.Dialogues
+{
+    public class LuciasWalkAI : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _points;
+        [SerializeField] private float _threshold = 0.2f;
+        
+        [SerializeField] private CheckInOnLayer _checkForPlayer;
+        
+        private NavMeshAgent _agent;
+        private int _currentPoint;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            
+            _checkForPlayer.EventOnTrue.AddListener(SetDestination);
+            _checkForPlayer.EventOnFalse.AddListener(Stop);
+        }
+
+        private void SetDestination()
+        {
+            _agent.destination = _points[_currentPoint].position;
+            _agent.isStopped = false;
+        }
+
+        private void Stop()
+        {
+            _agent.isStopped = true;
+        }
+
+        private async void CheckIsOnPosition()
+        {
+            while (transform.position.magnitude > _threshold)
+            {
+                Task.Yield();
+            }
+            
+            Stop();
+        }
+    }
+}
